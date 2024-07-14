e20d991dc0f1f04c4eb6393aad9edc46e0fc225e
Alex Ullate <aullate2508@gmail.com>
2024-07-14 11:18:53 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shared_utils",
+    version="0.1.0",
+    description="Shared utils",
+    author="Alex Ullate",
+    author_email="aullate2508@gmail.com",
+    packages=find_packages(),
+)
---
+++ shared_utils/text.py
@@ -0,0 +1,5 @@
+class Text:
+
+    @staticmethod
+    def count_characters(text: str) -> int:
+        return len(text)
